98c52086d0c90ae84f31bf6740bd95d0e40d6613
Florian Lenz <flenz81@googlemail.com>
2018-04-06 13:26:54 +0200

    [providers] added web socket provider

---
+++ providers/web-socket-provider.js
@@ -0,0 +1,101 @@
+'use strict';
+
+// See: https://github.com/ethereum/wiki/wiki/JSON-RPC
+
+var Provider = require('./json-rpc-provider.js');
+var WebSocket = require('ws');
+var EventEmitter = require('events');
+var uid = require('unique-string');
+
+var utils = (function() {
+    return {
+        defineProperty: require('../utils/properties').defineProperty
+    }
+})();
+
+function getResult(payload) {
+    if (payload.error) {
+        var error = new Error(payload.error.message);
+        error.code = payload.error.code;
+        error.data = payload.error.data;
+        throw error;
+    }
+
+    return payload.result;
+}
+
+function WebSocketProvider(url, network) {
+    if (!(this instanceof WebSocketProvider)) { throw new Error('missing new'); }
+    var self = this;
+
+    if (arguments.lengt == 1) {
+        if (typeof(url) === 'string') {
+            try {
+                network = Provider.getNetwork(url);
+                url = null;
+            } catch (error) { }
+        } else {
+            network = url;
+            url = null;
+        }
+    }
+
+    Provider.call(this, network);
+
+    utils.defineProperty(this, 'eventEmitter', new EventEmitter());
+    utils.defineProperty(this, 'webSocketPromise', new Promise(function (res, rej) {
+        var ws = new WebSocket(url);
+
+        ws.on('error', rej);
+        ws.on('close', rej);
+        ws.on('open', function () {
+            res(ws)
+        });
+
+        ws.on('message', function (message) {
+            var msg = JSON.parse(message);
+            self.eventEmitter.emit(msg.id, message);
+        });
+
+    }));
+
+}
+Provider.inherits(WebSocketProvider);
+
+WebSocketProvider.prototype.rpcRequestCount = 0;
+
+utils.defineProperty(WebSocketProvider.prototype, 'fetchJSON', function (webSocketPromise, json, processFunc) {
+    var self = this;
+    return new Promise(function (res, rej) {
+
+        json.id = uid();
+
+        self
+            .webSocketPromise
+            .then(function (webSocket) {
+
+                //Event handler for event emitter
+                var eventHandler = function (data) {
+                    //Remove event listener to keep the amount low
+                    self.eventEmitter.removeListener(json.id, eventHandler);
+                    res(getResult(JSON.parse(data)))
+                };
+
+                self.eventEmitter.on(json.id, eventHandler);
+
+                webSocket.send(JSON.stringify(json))
+            })
+            .catch(rej);
+    })
+});
+
+utils.defineProperty(WebSocketProvider.prototype, 'send', function(method, params) {
+    var request = {
+        method: method,
+        params: params,
+        jsonrpc: "2.0"
+    };
+    return this.fetchJSON(this.webSocketPromise, request, getResult);
+});
+
+module.exports = WebSocketProvider;
